e52b4c1ed3c5f74dc75fb3af89e054f57266e870
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2026-04-04 06:09:29 +0700

    feat: hello world!

--- src_bootstrap/zeus.c
+++ src_bootstrap/zeus.c
@@ -731,6 +731,10 @@ static void lower_fn(Fn *f) {
     for (size_t i = 0; i < f->body_count; ++i) {
         lower_stmt(f->body[i]);
     }
+    if (strcmp(fn->name, "main") == 0) {
+        bool has_ret = fn->count > 0 && fn->inst[fn->count - 1].kind == I_Ret;
+        if (!has_ret) emit(I_Ret, op_const(0.0f), (IROp){0}, (IROp){0});
+    }
 }
 
 static void lower_prog() {
@@ -840,6 +844,78 @@ static void dedup() {
     }
 }
 
+static void get_dir(const char *path, char *dir, size_t size) {
+    const char *slash = strchr(path, '/');
+    if (slash) {
+        size_t len = slash - path;
+        snprintf(dir, size, "%.*s", (int)len, path);
+    } else snprintf(dir, size, ".");
+}
+
+static void emit_inst(FILE *f, IRInst *i) {
+    if (i->kind == I_LoadStr) {
+        fprintf(f, "  mov rdi, const_s%d\n", pool_str(i->l.v.str));
+    } else if (i->kind == I_PrintStr) {
+        fprintf(f, "  xor eax, eax\n");
+        fprintf(f, "  call puts\n");
+    } else if (i->kind == I_Ret) {
+        if (i->dest.kind == O_Const) fprintf(f, "  mov eax, %d\n", (int)i->dest.v.constant);
+        else fprintf(f, "  mov eax, 0\n");
+        fprintf(f, "  leave\n");
+        fprintf(f, "  ret\n");
+    }
+}
+
+static void emit_fn(FILE *f, IRFn *fn) {
+    fprintf(f, "%s:\n", fn->name);
+    fprintf(f, "  push rbp\n");
+    fprintf(f, "  mov rbp, rsp\n");
+
+    int stack = fn->next_tmp * 8;
+    if (stack % 16 != 0) stack += 8;
+    if (stack > 0) fprintf(f, "  sub rsp, %d\n", stack);
+
+    for (size_t i = 0; i < fn->count; ++i) emit_inst(f, &fn->inst[i]);
+
+    fprintf(f, "\n");
+}
+
+static void codegen() {
+    char dir[512];
+    get_dir(source_path, dir, sizeof(dir));
+    char asm_path[512];
+    char obj_path[512];
+    char exe_path[512];
+    snprintf(asm_path, sizeof(asm_path), "%s/out.asm", dir);
+    snprintf(obj_path, sizeof(obj_path), "%s/out.o", dir);
+    snprintf(exe_path, sizeof(exe_path), "%s/out", dir);
+
+    FILE *f = fopen(asm_path, "w");
+
+    fprintf(f, "SECTION .rodata\n");
+    for (size_t i = 0; i < pool.f_count; ++i)
+        fprintf(f, "  const_f%zu: dq %f\n",
+            i, pool.floats[i]);
+    for (size_t i = 0; i < pool.s_count; ++i)
+        fprintf(f, "  const_s%zu: db \"%s\", 0\n",
+            i, pool.strings[i]);
+    fprintf(f, "  fmt_float: db \"%%g\", 10, 0\n\n");
+
+    fprintf(f, "SECTION .text\n");
+    fprintf(f, "  EXTERN puts\n");
+    fprintf(f, "  global main\n\n");
+
+    for (size_t i = 0; i < ir.count; ++i) emit_fn(f, &ir.fns[i]);
+
+    fclose(f);
+
+    char cmd[1024];
+    snprintf(cmd, sizeof(cmd), "nasm -felf64 -o %s %s", obj_path, asm_path);
+    if (system(cmd) != 0) return;
+    snprintf(cmd, sizeof(cmd), "gcc -no-pie -o %s %s", exe_path, obj_path);
+    if (system(cmd) != 0) return;
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Usage: %s <path/to/file.zeus>\n", argv[0]);
@@ -872,7 +948,8 @@ int main(int argc, char **argv) {
 
     lower_prog();
     dedup();
-    print_ir();
+    // print_ir();
+    codegen();
     return 0;
 }
 
